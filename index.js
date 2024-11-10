795a7a015cbc5878491694ef1d4825c35d0d8503
muhammad hassan <hassanshahid778866@gmail.com>
2024-11-10 12:32:38 +0500

    Add files via upload

---
+++ index.js
@@ -0,0 +1,7 @@
+function chekperc() {
+    let zkt = document.getElementById("tm1").value;
+    let res = zkt * 0.025
+    document.getElementById("g1").innerHTML=("your zakat amount is" + res )
+
+
+}
